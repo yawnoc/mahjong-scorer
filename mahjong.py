e5562f9352878245becde585af9a5103297395f9
Conway <yawnoc@pm.me>
2023-06-11 15:42:56 +0800

    Create mahjong.py

---
+++ mahjong.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+# mahjong.py
+
+A Python scorer for Mahjong.
+
+Copyright 2023 Conway
+Licensed under MIT No Attribution (MIT-0), see LICENSE.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
