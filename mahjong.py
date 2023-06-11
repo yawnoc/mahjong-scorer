f06aca62122defc3f801281b6e21deba2e3d9bee
Conway <yawnoc@pm.me>
2023-06-11 16:01:53 +0800

    Implement reading of scores file

--- mahjong.py
+++ mahjong.py
@@ -10,6 +10,8 @@ Licensed under MIT No Attribution (MIT-0), see LICENSE.
 """
 
 import argparse
+import os
+import sys
 
 __version__ = '0.0.0'
 
@@ -32,9 +34,33 @@ def parse_command_line_arguments():
     return argument_parser.parse_args()
 
 
+def read_scores_text(scores_file_name):
+    if os.path.isdir(scores_file_name):
+        print(
+            f'Error: `{scores_file_name}` is a directory, not a file',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    try:
+        with open(scores_file_name, 'r', encoding='utf-8') as scores_file:
+            scores_text = scores_file.read()
+    except FileNotFoundError:
+        print(
+            f'Error: file `{scores_file_name}` not found',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    return scores_text
+
+
 def main():
     parsed_arguments = parse_command_line_arguments()
     scores_file_name = parsed_arguments.scores_file_name
+
+    scores_text = read_scores_text(scores_file_name)
+    print(scores_text)
 
 
 if __name__ == '__main__':
